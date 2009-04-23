baa1b5a0f19009c668ca56dd8564e30dc4cd24b9
Lame Spirale <contact@lame-spirale.net>
2009-04-23 17:01:36 +0200

    shape au lieu de cycle + commentaires

--- dict/admin.py
+++ dict/admin.py
@@ -1,20 +1,20 @@
+from c5.dict.models import Shape, Reading, GtrVoicing
 from django.contrib import admin
-from models import Cycle, Reading, GtrVoicing
-
-class CycleAdmin(admin.ModelAdmin):
-    fields = ('n1','n2')
-    list_display = ('n1','n2')
-
-class GtrVoicingAdmin(admin.ModelAdmin):
-    fields = ('cycle', 's1', 's2', 's3', 's4', 's5', 's6')
-    list_display = ('cycle', 's1', 's2', 's3', 's4', 's5', 's6')
 
 
-class ReadingAdmin(admin.ModelAdmin):
-    fields = ('rname' , 'rtype', 'fondamental', 'cycle', 'created')
-    list_display = ('rname' , 'rtype', 'fondamental', 'cycle', 'created')
+class ShapeAdmin(admin.ModelAdmin):
+    fields = 'shape_id'
+    list_display = 'shape_id'
+
+#class GtrVoicingAdmin(admin.ModelAdmin):
+#    fields = ('shape', 's1', 's2', 's3', 's4', 's5', 's6')
+#    list_display = ('shape', 's1', 's2', 's3', 's4', 's5', 's6')
 
 
-admin.site.register(Cycle, CycleAdmin)
-admin.site.register(Reading, ReadingAdmin)
-admin.site.register(GtrVoicing, GtrVoicingAdmin)
+#class ReadingAdmin(admin.ModelAdmin):
+#    fields = ('rname' , 'rtype', 'fondamental', 'shape', 'created')
+#    list_display = ('rname' , 'rtype', 'fondamental', 'shape', 'created')
+
+
+admin.site.register(Shape, ShapeAdmin)
+
--- dict/models.py
+++ dict/models.py
@@ -1,27 +1,33 @@
 from django.db import models
 
-class Cycle(models.Model):
+class Shape(models.Model):
+# Une shape est un dessin sur le cercle, soit groupe de notes
     def __unicode__(self):
-        return self.cycle_id
-    cycle_id = models.TextField(primary_key=True)
-
+        return self.shape_id
+    shape_id = models.TextField(primary_key=True)
+# shape_id représente les intervals : il sera en base-12, un chiffre par note.
+# Pour s'assurer qu'il n'y ai pas de doublons, on devra probablement toujours le transformer en sa "prime form"
 
 
 
 class Reading(models.Model):
-  #  def __unicode__(self):
-   #     return self.reading
+# Reading est une interprétation d'une shape. Ex : "Accord mineur"
+    def __unicode__(self):
+        return self.rname
     rname = models.CharField(max_length=100)
     rtype = models.CharField(max_length=10)
     fondamental = models.IntegerField()
-    cycle = models.ForeignKey(Cycle)
+    shape = models.ForeignKey(Shape)
     created = models.DateTimeField('Date de creation')
 
 
-class GtrVoicing(models.Model):
-   # def __unicode__(self):
-    #    return self.gtrvoicing
-    cycle = models.ForeignKey(Cycle)
+class Voicing(models.Model):
+    shape = models.ForeignKey(Shape)
+# Il faudrait déclarer les fonctions relatives au voicings ici, de facon à se préparer à éventuellement avoir un autre instrument (ex : guitare accordé en open)
+
+
+class GtrVoicing(Voicing):
+# Je suis pas sur si on stock chaque corde séparément, ou plutot un champ "string"
     s1 = models.IntegerField()
     s2 = models.IntegerField()
     s3 = models.IntegerField()
@@ -29,5 +35,3 @@ class GtrVoicing(models.Model):
     s5 = models.IntegerField()
     s6 = models.IntegerField()
 
-class Test(models.Model):
-	rname = models.CharField(max_length=100)
